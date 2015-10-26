94432f99d90185e071e75d4c848f9cc13671aacc
mohaider <mrhaider@gmail.com>
2015-10-26 17:25:38 -0400

    test file

---
+++ HEDDOKO_Unity_test_v0.0.3/Caoching Demo 0.0.1/Assets/Scripts/TBDeleted/CSVfileReader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Assets.Scripts.Utils;
+using System.Text;
+using UnityEngine;
+namespace Assets.Scripts.TBDeleted
+{
+    public class CSVfileReader : MonoBehaviour
+    {
+        void Start()
+        {
+
+            int t = BodyRecordingsMgr.Instance.ScanRecordings(FilePathReferences.CSVDirectory);
+            BodyRecordingsMgr.Instance.ReadAllRecordings();
+            string recGuid = "DA920FFD-D8D0-436D-979D-48C73031F9F2";
+            BodyFramesRecording bfr = BodyRecordingsMgr.Instance.GetRecordingByUUID(recGuid);
+            Body b = BodiesManager.Instance.GetBodyFromRecordingUUID(recGuid);
+            //b.InitBody(b.BodyGuid); //from the given body, initialize a new body, containing its segments. 
+
+            b.PlayRecording(recGuid);
+         
+        }
+    }
+}
