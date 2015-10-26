2e5d69939f313b4b084e16cdfa822ef180947cd8
mohaider <mrhaider@gmail.com>
2015-10-26 17:16:52 -0400

    class to help with matrix manipulation
    Class that helps with matrix manipulation

---
+++ HEDDOKO_Unity_test_v0.0.3/Caoching Demo 0.0.1/Assets/Scripts/Utils/MatrixTools.cs
@@ -0,0 +1,133 @@
+﻿
+
+using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Utils
+{
+    public static class MatrixTools
+    {
+
+
+        public static Quaternion MatToQuat(float[,] m)
+        {
+            Quaternion q;
+            q.w = (m[0, 0] + m[1, 1] + m[2, 2] + 1.0f) / 4.0f;
+            q.x = (m[0, 0] - m[1, 1] - m[2, 2] + 1.0f) / 4.0f;
+            q.y = (-m[0, 0] + m[1, 1] - m[2, 2] + 1.0f) / 4.0f;
+            q.z = (-m[0, 0] - m[1, 1] + m[2, 2] + 1.0f) / 4.0f;
+            if (q.w < 0.0f) q.w = 0.0f;
+            if (q.x < 0.0f) q.x = 0.0f;
+            if (q.y < 0.0f) q.y = 0.0f;
+            if (q.z < 0.0f) q.z = 0.0f;
+            q.w = (float)Math.Sqrt(q.w);
+            q.x = (float)Math.Sqrt(q.x);
+            q.y = (float)Math.Sqrt(q.y);
+            q.z = (float)Math.Sqrt(q.z);
+            if (q.w >= q.x && q.w >= q.y && q.w >= q.z)
+            {
+                q.w *= +1.0f;
+                q.x *= Mathf.Sign(m[2, 1] - m[1, 2]);
+                q.y *= Mathf.Sign(m[0, 2] - m[2, 0]);
+                q.z *= Mathf.Sign(m[1, 0] - m[0, 1]);
+            }
+            else if (q.x >= q.w && q.x >= q.y && q.x >= q.z)
+            {
+                q.w *= Mathf.Sign(m[2, 1] - m[1, 2]);
+                q.x *= +1.0f;
+                q.y *= Mathf.Sign(m[1, 0] + m[0, 1]);
+                q.z *= Mathf.Sign(m[0, 2] + m[2, 0]);
+            }
+            else if (q.y >= q.w && q.y >= q.x && q.y >= q.z)
+            {
+                q.w *= Mathf.Sign(m[0, 2] - m[2, 0]);
+                q.x *= Mathf.Sign(m[1, 0] + m[0, 1]);
+                q.y *= +1.0f;
+                q.z *= Mathf.Sign(m[2, 1] + m[1, 2]);
+            }
+            else if (q.z >= q.w && q.z >= q.x && q.z >= q.y)
+            {
+                q.w *= Mathf.Sign(m[1, 0] - m[0, 1]);
+                q.x *= Mathf.Sign(m[2, 0] + m[0, 2]);
+                q.y *= Mathf.Sign(m[2, 1] + m[1, 2]);
+                q.z *= +1.0f;
+            }
+
+            float r = (float)Math.Sqrt(q.w * q.w + q.x * q.x + q.y * q.y + q.z * q.z);
+            q.w /= r;
+            q.x /= r;
+            q.y /= r;
+            q.z /= r;
+            return q;
+        }
+        //////////////////////////// These are the reference functions for matrix calculation and transformation ///////////////////////////////
+
+        /**
+        //	* RotationGlobal()
+        //	* @ This Performs transformation From Global Coordinate System To local Nod coordinates
+        //	* @params yaw pitch and roll of each nod
+        //	* @return 3*3 orientation matrix in nod local coordinate system
+        //	*/
+        // 
+        public static float[,] RotationGlobal(float yaw, float pitch, float roll)
+        {
+            float[,] a = new float[3, 3];
+            a[0, 0] = Mathf.Cos(pitch) * Mathf.Cos(yaw);
+            a[1, 0] = (Mathf.Sin(roll) * Mathf.Cos(yaw) * Mathf.Sin(pitch) - Mathf.Sin(yaw) * Mathf.Cos(roll));
+            a[2, 0] = (Mathf.Sin(roll) * Mathf.Sin(yaw) + Mathf.Cos(yaw) * Mathf.Sin(pitch) * Mathf.Cos(roll));
+            a[0, 1] = Mathf.Cos(pitch) * Mathf.Sin(yaw);
+            a[1, 1] = Mathf.Sin(roll) * Mathf.Sin(yaw) * Mathf.Sin(pitch) + Mathf.Cos(yaw) * Mathf.Cos(roll);
+            a[2, 1] = (Mathf.Cos(roll) * Mathf.Sin(yaw) * Mathf.Sin(pitch) - Mathf.Cos(yaw) * Mathf.Sin(roll));
+            a[0, 2] = -Mathf.Sin(pitch);
+            a[1, 2] = Mathf.Cos(pitch) * Mathf.Sin(roll);
+            a[2, 2] = (Mathf.Cos(pitch) * Mathf.Cos(roll));
+            return a;
+        }
+
+
+
+        /**
+        //	* RotationLocal()
+        //	* @ This Performs transformation From Nods Local Coordinate System To global coordinates
+        //	* @params yaw pitch and roll of each nod
+        //	* @return 3*3 orientation matrix in global coordinate system
+        //	*/
+        // 
+        public static float[,] RotationLocal(float yaw, float pitch, float roll)
+        {
+            float[,] a = new float[3, 3];
+            a[0, 0] = Mathf.Cos(pitch) * Mathf.Cos(yaw);
+            a[1, 0] = Mathf.Cos(pitch) * Mathf.Sin(yaw);
+            a[2, 0] = -Mathf.Sin(pitch);
+            a[0, 1] = -Mathf.Cos(roll) * Mathf.Sin(yaw) + Mathf.Cos(yaw) * Mathf.Sin(pitch) * Mathf.Sin(roll);
+            a[1, 1] = Mathf.Sin(roll) * Mathf.Sin(yaw) * Mathf.Sin(pitch) + Mathf.Cos(yaw) * Mathf.Cos(roll);
+            a[2, 1] = Mathf.Cos(pitch) * Mathf.Sin(roll);
+            a[0, 2] = (Mathf.Sin(roll) * Mathf.Sin(yaw) + Mathf.Cos(yaw) * Mathf.Sin(pitch) * Mathf.Cos(roll));
+            a[1, 2] = (Mathf.Cos(roll) * Mathf.Sin(yaw) * Mathf.Sin(pitch) - Mathf.Cos(yaw) * Mathf.Sin(roll));
+            a[2, 2] = (Mathf.Cos(pitch) * Mathf.Cos(roll));
+            return a;
+        }
+
+        /**
+//	* multi()
+//	*	@This Function do multiplication between two 3*3 matrices
+//	*	@param matrix a and b
+//	*	@return c = a * b,
+//	*/
+        public static float[,] multi(float[,] a, float[,] b)
+        {
+            float[,] c = new float[3, 3];
+            int i, j, k;
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    c[i, j] = a[i, 0] * b[0, j] + a[i, 1] * b[1, j] + a[i, 2] * b[2, j];
+
+                }
+            }
+            return c;
+        }
+
+    }
+}
