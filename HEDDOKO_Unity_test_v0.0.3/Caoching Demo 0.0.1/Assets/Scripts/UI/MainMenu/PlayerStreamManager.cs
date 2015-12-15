48bba65f788996a11730d5f4a2fca222d50a9a2a
chepundup <mazen.elbawab@gmail.com>
2015-12-15 10:10:59 -0500

    updated the testing recordings

--- HEDDOKO_Unity_test_v0.0.3/Caoching Demo 0.0.1/Assets/Scripts/UI/MainMenu/PlayerStreamManager.cs
+++ HEDDOKO_Unity_test_v0.0.3/Caoching Demo 0.0.1/Assets/Scripts/UI/MainMenu/PlayerStreamManager.cs
@@ -22,7 +22,8 @@ namespace Assets.Scripts.UI.MainMenu
     {
         public Body CurrentBodyInPlay { get; set; }
         private BodyPlaybackState mCurrentState = BodyPlaybackState.Waiting;
-      //  private bool mPlayButtonPushed; 
+        
+        //  private bool mPlayButtonPushed; 
         public float PauseThreadTimer = 1f;
         private float mInternalTimer = 1f;
 
@@ -95,13 +96,11 @@ namespace Assets.Scripts.UI.MainMenu
         */
         public void Play()
         {
-           
-                if (CurrentBodyInPlay != null)
-                {
-                   // mPlayButtonPushed = true; 
-                    ChangeState(BodyPlaybackState.PlayingRecording); 
-                }
-           
+            if (CurrentBodyInPlay != null)
+            {
+                // mPlayButtonPushed = true; 
+                ChangeState(BodyPlaybackState.PlayingRecording); 
+            }
         }
 
         /// <summary>
