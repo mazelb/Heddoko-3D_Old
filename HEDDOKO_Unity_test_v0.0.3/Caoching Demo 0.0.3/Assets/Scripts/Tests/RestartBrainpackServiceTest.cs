c4006f9a1da4e0746c7fa731eafde07a1abc0718
fatemeag <f.aghazadeh87@gmail.com>
2016-03-30 14:54:36 -0400

    calibration debugging ...

--- HEDDOKO_Unity_test_v0.0.3/Caoching Demo 0.0.3/Assets/Scripts/Tests/RestartBrainpackServiceTest.cs
+++ HEDDOKO_Unity_test_v0.0.3/Caoching Demo 0.0.3/Assets/Scripts/Tests/RestartBrainpackServiceTest.cs
@@ -1,95 +1,95 @@
-﻿/*@file RestartBrainpackServiceTest.cs
-* @brief Contains the RestartBrainpackServiceTest class
-* @author Mohammed Haider(mohammed@heddoko.com)
-* @date March 2016
-* Copyright Heddoko(TM) 2016, all rights reserved
-*/
+﻿///*@file RestartBrainpackServiceTest.cs
+//* @brief Contains the RestartBrainpackServiceTest class
+//* @author Mohammed Haider(mohammed@heddoko.com)
+//* @date March 2016
+//* Copyright Heddoko(TM) 2016, all rights reserved
+//*/
 
-using System;
-using System.Collections;
-using System.ServiceProcess;
-using UnityEngine;
+//using System;
+//using System.Collections;
+//using System.ServiceProcess;
+//using UnityEngine;
 
-namespace Assets
-{
+//namespace Assets
+//{
 
-    public class RestartBrainpackServiceTest : MonoBehaviour
-    {
-        private ServiceController mServiceController;
+//    public class RestartBrainpackServiceTest : MonoBehaviour
+//    {
+//        private ServiceController mServiceController;
 
-        public void Awake()
-        {
-            mServiceController = new ServiceController("BrainpackService");
-            RestartBrainpackService();
-        }
-        public void RestartBrainpackService()
-        {
-            StartCoroutine(RestartBpService(2));
-        }
+//        public void Awake()
+//        {
+//            mServiceController = new ServiceController("BrainpackService");
+//            RestartBrainpackService();
+//        }
+//        public void RestartBrainpackService()
+//        {
+//            StartCoroutine(RestartBpService(2));
+//        }
 
-        /// <summary>
-        /// Restart the bp service. Timeout before failing
-        /// </summary>
-        /// <param name="vTimeout"></param>
-        /// <returns></returns>
-        private IEnumerator RestartBpService(float vTimeout)
-        {
+//        /// <summary>
+//        /// Restart the bp service. Timeout before failing
+//        /// </summary>
+//        /// <param name="vTimeout"></param>
+//        /// <returns></returns>
+//        private IEnumerator RestartBpService(float vTimeout)
+//        {
 
-            float vRemainingTime = vTimeout;
+//            float vRemainingTime = vTimeout;
 
-            bool mStopSuccess = false;
-            bool mServiceStartSuccess = false;
-            try
-            {
-                mServiceController.Stop();
-                mStopSuccess = true;
-            }
-            catch (Exception vE)
-            {
+//            bool mStopSuccess = false;
+//            bool mServiceStartSuccess = false;
+//            try
+//            {
+//                mServiceController.Stop();
+//                mStopSuccess = true;
+//            }
+//            catch (Exception vE)
+//            {
 
-                Debug.Log("Problem stopping" + vE);
-            }
+//                Debug.Log("Problem stopping" + vE);
+//            }
 
-            if (mStopSuccess)
-            {
-                while (vRemainingTime >= 0)
-                {
-                    vRemainingTime -= Time.time;
-                    //check status
-                    if (mServiceController.Status == ServiceControllerStatus.Stopped)
-                    {
-                        Debug.Log("Service stopped.");
-                        yield break;
-                    }
-                    yield return null;
-                }
-            }
-            try
-            {
-                mServiceController.Start();
-                mServiceStartSuccess = true;
-                vRemainingTime = vTimeout;
-            }
-            catch (Exception vE)
-            {
+//            if (mStopSuccess)
+//            {
+//                while (vRemainingTime >= 0)
+//                {
+//                    vRemainingTime -= Time.time;
+//                    //check status
+//                    if (mServiceController.Status == ServiceControllerStatus.Stopped)
+//                    {
+//                        Debug.Log("Service stopped.");
+//                        yield break;
+//                    }
+//                    yield return null;
+//                }
+//            }
+//            try
+//            {
+//                mServiceController.Start();
+//                mServiceStartSuccess = true;
+//                vRemainingTime = vTimeout;
+//            }
+//            catch (Exception vE)
+//            {
 
-                Debug.Log("Problem starting" + vE);
-            }
-            if (mServiceStartSuccess)
-            {
-                while (vRemainingTime >= 0)
-                {
-                    vRemainingTime -= Time.time;
-                    //check status
-                    if (mServiceController.Status == ServiceControllerStatus.Running)
-                    {
-                        Debug.Log("Service started again.");
-                        yield break;
-                    }
-                    yield return null;
-                }
-            }
-        }
+//                Debug.Log("Problem starting" + vE);
+//            }
+//            if (mServiceStartSuccess)
+//            {
+//                while (vRemainingTime >= 0)
+//                {
+//                    vRemainingTime -= Time.time;
+//                    //check status
+//                    if (mServiceController.Status == ServiceControllerStatus.Running)
+//                    {
+//                        Debug.Log("Service started again.");
+//                        yield break;
+//                    }
+//                    yield return null;
+//                }
+//            }
+//        }
 
-    }
-}
+//    }
+//}
